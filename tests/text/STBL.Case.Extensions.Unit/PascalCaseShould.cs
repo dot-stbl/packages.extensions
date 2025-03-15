f474f80526a937514cc3f316ac4a0100df967c0f
Alexander Sergeev <lowern1ght@yahoo.com>
2025-03-15 19:33:19 +0500

    [pl](release): STBL.Case.Extensions.csproj package

---
+++ tests/text/STBL.Case.Extensions.Unit/PascalCaseShould.cs
@@ -0,0 +1,27 @@
+using FluentAssertions;
+
+namespace STBL.Case.Extensions.Unit;
+
+public class StringExtensionsToPascalCase
+{
+    [Theory]
+    [InlineData(null)]
+    public void ReturnArgumentNullException(string source)
+    {
+        Action act = () => source.ToPascalCase();
+        act.Should().Throw<ArgumentNullException>();
+    }
+
+    [Theory]
+    [InlineData("api/users/32/someActionToDo?param=%a%")]
+    [InlineData("Api/Users/32/SomeActionToDo?Param=%A%")]
+    [InlineData("api/users/32/some-action-to-do?param=%a%")]
+    [InlineData("api/users/32/Some-Action-To-Do?Param=%a%")]
+    [InlineData("api/users/32/some_action_to_do?param=%a%")]
+    [InlineData("api/users/32/Some_Action_to_do?param=%a%")]
+    public void ReturnPascalCaseUrl(string source)
+    {
+        var expectedResult = "Api/Users/32/SomeActionToDo?Param=%A%";
+        source.ToPascalCase().Should().Be(expectedResult);
+    }
+}
